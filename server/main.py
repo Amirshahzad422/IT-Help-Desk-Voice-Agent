43941774a6eb35bd77a3c19e71c4a487ace31608
Sajeela-25 <sajeelaafaqabbasi@gmail.com>
2026-08-07 02:25:55 +0500

    Add Vite configuration and project files

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
 import os
 import uuid
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 from livekit import api
+from livekit.api import LiveKitAPI
+from livekit.protocol.room import ListRoomsRequest
+
 
 load_dotenv()
 
@@ -10,15 +14,32 @@ app = FastAPI(
     title="IT Help Desk Voice Agent Token Server"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 LIVEKIT_URL = os.getenv("LIVEKIT_URL")
 LIVEKIT_API_KEY = os.getenv("LIVEKIT_API_KEY")
 LIVEKIT_API_SECRET = os.getenv("LIVEKIT_API_SECRET")
-
 
 if not LIVEKIT_API_KEY or not LIVEKIT_API_SECRET:
     raise RuntimeError(
         "LiveKit API credentials are missing. Check your .env file."
     )
+
+
+# LiveKit server API client
+livekit_api = LiveKitAPI(
+    url=LIVEKIT_URL,
+    api_key=LIVEKIT_API_KEY,
+    api_secret=LIVEKIT_API_SECRET
+)
 
 
 @app.get("/")
@@ -29,12 +50,26 @@ def health_check():
 
 
 @app.post("/token")
-def generate_token():
+async def generate_token():
     try:
-        # Generate a unique room name
-        room_name = f"helpdesk-{uuid.uuid4().hex[:8]}"
+        # Get currently active rooms
+        rooms_response = await livekit_api.room.list_rooms(
+            ListRoomsRequest()
+        )
 
-        # Generate a unique participant identity
+        active_rooms = [
+            room.name
+            for room in rooms_response.rooms
+        ]
+
+        # Generate room name and avoid collisions
+        while True:
+            room_name = f"helpdesk-{uuid.uuid4().hex[:8]}"
+
+            if room_name not in active_rooms:
+                break
+
+        # Generate participant identity
         identity = f"user-{uuid.uuid4().hex[:6]}"
 
         # Create LiveKit JWT token
